9a969b15d61ead45ef969df9057ad1e8c0dcf295
DESKTOP-DMC8UIV\renzo <renzofpanza@gmail.com>
2025-04-09 17:42:00 -0300

    Testando if's nos tokens (dando erro)

--- chatBot.py
+++ chatBot.py
@@ -2,75 +2,37 @@ import nltk
 import random
 from nltk.chat.util import Chat, reflections
 from nltk.tokenize import sent_tokenize, word_tokenize
+import unicodedata
 
+respostas = ["Nós temos livros de ação", "Nós temos livros de terror"] ## pelo pares
 
-perguntas = ["Olá! Você tem livros de ação?", "Ola! Eu quero comprar um livro!", "Ola! Você tem livros de terror?"]
+perguntas_user = input()
 
-respostas = ["Nós temos livros de ação", "Nós temos livros de terror"]
+def tokenizar_perguntas_usuario(perguntas_user, respostas):
+    token_sentenca = sent_tokenize(perguntas_user)
+    print(token_sentenca)
 
+    for token in token_sentenca:
+        token_palavra_pergunta = word_tokenize(token)
+        print(token_palavra_pergunta)
 
-# def tokenizar_perguntas(perguntas):
-#     for setencas in perguntas:
-#         token = sent_tokenize(setencas)
-#         print('FOR para tokenizar por sentença')
-#         print(token)
-#         print(len(token))
+    # for sentencas_respostas in respostas:
+    #     token_respostas = sent_tokenize(sentencas_respostas)
 
-#         for palavras in token:
-#             token_palavras = word_tokenize(palavras)
+    #     for palavras_respostas in token_respostas:
+    #         token_palavras_respostas = word_tokenize(palavras_respostas)
 
-#             print('FOR para tokenizar por palavra')
-#             print(token_palavras)
-#             print(len(token_palavras))
+    if {"quero", "livro"}.issubset(token_palavra_pergunta):
+        return "Entendi! Me explique mais a respeito do gênero e do livro!"
+    elif{"terror"}.issubset(token_palavra_pergunta):
+        return "Nós temos livros de terror! Qual tipo de terror você curte?"
+    return None
 
-# def tokenizar_respostas(respostas):
-#     for sentencas_respostas in respostas:
-#         token_respostas = sent_tokenize(sentencas_respostas)
-#         print('FOR para tokenizar por sentença')
-#         print(token_respostas)
-#         print(len(token_respostas))
+tokenizar_perguntas_usuario(perguntas_user, respostas)
 
-#         for palavras_respostas in token_respostas:
-#             token_palavras_respostas = word_tokenize(palavras_respostas)
-#             print('FOR para tokenizar por palavra')
-#             print(token_palavras_respostas)
-#             print(len(token_palavras_respostas))
-
-def escolher_resposta(perguntas, respostas):
-    for setencas in perguntas:
-        token = sent_tokenize(setencas)
-        # print('FOR para tokenizar por sentença')
-        # print(token)
-        # print(len(token))
-
-        for palavras in token:
-            token_palavras = word_tokenize(palavras)
-
-            # print('FOR para tokenizar por palavra')
-            # print(token_palavras)
-            # print(len(token_palavras))
-
-    for sentencas_respostas in respostas:
-        token_respostas = sent_tokenize(sentencas_respostas)
-        # print('FOR para tokenizar por sentença')
-        # print(token_respostas)
-        # print(len(token_respostas))
-
-        for palavras_respostas in token_respostas:
-            token_palavras_respostas = word_tokenize(palavras_respostas)
-            # print('FOR para tokenizar por palavra')
-            # print(token_palavras_respostas)
-            # print(len(token_palavras_respostas))
-    
-    # has_any_match = any(palavra in token_palavras_respostas for palavra in token_palavras)
-    # print(has_any_match)
-
-    # if has_any_match:
-    for palavra in token_palavras_respostas:
-        if palavra in token_palavras:
-            print(palavra)
-
-
-escolher_resposta(perguntas, respostas)
-# print(type(perguntas))
-# print(len(perguntas))
+while True:
+    resposta_token = tokenizar_perguntas_usuario(perguntas_user, respostas)
+    if resposta_token:
+        print("ChatBot: ", resposta_token)
+    else:
+        print("ChatBot: Desculpe, não entendi.")
