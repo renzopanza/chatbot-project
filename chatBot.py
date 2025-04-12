d2d2b463585465dad93610b8a8d6dca967aff33b
Wamberg Nascimento <wambergnascimento@gmail.com>
2025-04-11 22:53:28 -0300

    adição de uma lista de pares e criação da logica de uso dos pares com o Chat()

--- chatBot.py
+++ chatBot.py
@@ -1,12 +1,16 @@
 import random
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.chat.util import Chat,reflections
+from pares import pares_lista
 
-respostas_chatBot = {
-    "ação": "Sim! Temos ótimos livros de ação. Você prefere algo mais moderno ou clássico?",
-    "terror": "Temos muitos livros de terror. Gosta mais de terror psicológico ou sobrenatural?",
-    "romance": "Claro! Nossos romances são bem variados. Você gosta de histórias contemporâneas ou de época?",
-    "livro": "Temos vários gêneros. Está procurando algo leve ou mais intenso?",
+
+respostas_chatBot = { # >>>> aqui vamos precisar de tokens mais especificos(como "não sei","estou indeciso" etc), deixando o os gêneros literarios para serem tratados nos pares
+    
+    # "ação": "Sim! Temos ótimos livros de ação. Você prefere algo mais moderno ou clássico?",
+    # "terror": "Temos muitos livros de terror. Gosta mais de terror psicológico ou sobrenatural?",
+    # "romance": "Claro! Nossos romances são bem variados. Você gosta de histórias contemporâneas ou de época?",
+    # "livro": "Temos vários gêneros. Está procurando algo leve ou mais intenso?",
 }
 
 # perguntas_chatBot = [
@@ -18,7 +22,7 @@ respostas_chatBot = {
 
 # perguntas_user = input()
 
-def responder(usuario_input):
+def responder_personalizada(usuario_input): # >>>> essa função permanece quase igual(agora ela retorna None se não houver resposta adequada nos tokens). Ela vai controlar as respostas com tokens quando tivermos
     tokens_usuario = word_tokenize(usuario_input.lower())
     
     for palavras_chaves in respostas_chatBot:
@@ -28,15 +32,47 @@ def responder(usuario_input):
             # return f"{resposta} {nova_pergunta}"
             return f"{resposta_chat}"
     
-    return "Não entendi muito bem. Poderia repetir de outra maneira?"
+    return None
 
 # Loop do chatbot
-print("ChatBot: Olá! Eu posso te ajudar a encontrar diversos livro.Qual tipo de livro você procura?")
+print("ChatBot: Olá! Eu posso te ajudar a encontrar diversos livro.Qual gênero literário você procura?")
+
+# while True:
+#     entrada_usuario = input("Você: ")
+#     resposta_chat = responder(entrada_usuario)
+#     print("ChatBot:", resposta_chat)
+
+reflections = { # >>>> por mais que não usemos agora, coloquei aqui para poder instanciar um objeto 'Chat' do nltk, que recebe reflections como parametro
+    "eu":"você",
+    "meu":"seu",
+    "meus":"seus",
+    "sou":"é",
+    "estou":"está",
+    "fui":"foi",
+    "era":"foi",
+    "você":"eu",
+    "você é":"eu sou",
+    "você está":"eu estou",
+}
+
+pares = pares_lista
+
+chatbot = Chat(pares, reflections)
 
 while True:
-    entrada_usuario = input("Você: ")
-    resposta_chat = responder(entrada_usuario)
-    print("ChatBot:", resposta_chat)
+    entrada_usuario = input("Voce: ")
+    if entrada_usuario == "sair":
+        print("ChatBook: Adeus! Se precisar de algo relacionado a livros novamente pode contar comigo!")
+        break
+    resposta_token = responder_personalizada(entrada_usuario)
+    if resposta_token: # >>>> prioriza respostas com token. Caso não tenha nenhuma entra no else e usa os pares
+        print("ChatBook: ", resposta_token)
+    else:
+        resposta_padrao = chatbot.respond(entrada_usuario)
+        if resposta_padrao:
+            print("ChatBook: ", resposta_padrao)
+        else:
+            print("ChatBook: Desculpe, não entendi. Pode responder de outra forma por favor?")
 
 # def tokenizar_perguntas_usuario(perguntas_user, respostas):
 #     token_sentenca = sent_tokenize(perguntas_user)
---
+++ pares.py
@@ -0,0 +1,211 @@
+pares_lista = [
+
+    [
+    r"(.*)fic[cç][ãa]o cient[íi]fica(.*)|(.*)sifi.?|ficção|sci fy.?|(.*)scify.?",
+    [
+    "Claro! Ficção cientifica é realmente um dos gêneros mais interessantes e instigantes que existe, pois nos faz pensar sobre os limites da ciência e o que seria possìvel na realidade. Mas me diga, na ficção científica você prefere viagens no tempo, contato com extraterrestres ou futuros distópicos?"
+    ]
+    ],
+        [
+        r"(.*)viagens no tempo(.*)",
+        ["Viagens no tempo sempre rendem histórias instigantes! Você prefere histórias com paradoxos temporais ou mudanças na linha do tempo?",
+        "Ah, histórias de viagens no tempo são fascinantes! Você gosta mais de histórias com paradoxos temporais ou mudanças na linha do tempo?"]
+        ],
+            [
+            r"(.*)paradoxos temporais(.*)|(.*)paradoxos(.*)",
+            ["Paradoxos temporais sempre desafiam a lógica! Você prefere histórias onde o personagem se encontra com ele mesmo ou onde muda eventos e cria realidades alternativas?",
+            "Esses dilemas temporais são geniais! Você curte mais histórias onde o personagem se encontra com ele mesmo ou onde muda eventos e cria realidades alternativas?"]
+            ],
+                [
+                r"(.*)encontra com ele mesmo(.*)|(.*)encontra com ele(.*)",
+                ["Encontros consigo mesmo são sempre carregados de tensão e reflexão. Você prefere quando isso gera autoconhecimento ou quando provoca colapsos temporais perigosos?",
+                "Esses encontros levantam dilemas pessoais profundos. Você curte mais quando isso gera autoconhecimento ou quando provoca colapsos temporais perigosos?"]
+                ],
+                    [
+                    r"(.*)gera autoconhecimento(.*)",
+                    ["Histórias que despertam autoconhecimento são sempre poderosas e emocionantes. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)colapsos temporais(.*)|(.*)colapsos(.*)",
+                    ["Colapsos temporais deixam tudo à beira do caos! Uma escolha intensa. Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)realidades alternativas(.*)",
+                ["Realidades alternativas abrem infinitas possibilidades! Você prefere histórias em que o personagem tenta voltar à linha original ou quando aceita viver nessa nova realidade?",
+                "Essas bifurcações de tempo são fascinantes. Você curte mais histórias em que o personagem tenta voltar à linha original ou quando aceita viver nessa nova realidade?"]
+                ],
+                    [
+                    r"(.*)tenta voltar à linha original(.*)|(.*)voltar(.*)|(.*)linha original(.*)",
+                    ["Personagens que tentam consertar o tempo mostram determinação e sacrifício. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)aceita viver nessa nova realidade(.*)|(.*)na nova(.*)|(.*)nova realidade(.*)",
+                    ["Aceitar uma nova realidade exige coragem e adaptação. Um caminho cheio de possibilidades. Qual outro gênero você gosta?"]
+                    ],
+            [
+            r"(.*)mudan[çc]as na linha do tempo(.*)",
+            ["Mudar a linha do tempo sempre traz consequências imprevisíveis! Você gosta mais de tramas em que o protagonista tenta salvar alguém ou mudar o rumo da história?",
+            "Essas histórias mexem com o destino das coisas. Você prefere quando o protagonista tenta salvar alguém ou mudar o rumo da história?"]
+            ],
+            [
+                r"(.*)salvar algu[eé]m(.*)",
+                ["Tentar salvar alguém é sempre emocionante e perigoso! Você prefere quando tudo dá errado e vira tragédia ou quando há um final surpreendentemente positivo?",
+                "Essas histórias mexem com emoções fortes. Você curte mais quando tudo dá errado e vira tragédia ou quando há um final surpreendentemente positivo?"]
+                ],
+                    [
+                    r"(.*)tudo dá errado.*trag[eé]dia(.*)d[aá] errado(.*)",
+                    ["Final trágico? Isso sempre deixa uma marca profunda. Que escolha intensa! Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)surpreendentemente positivo(.*)|(.*)final positivo(.*)",
+                    ["Reviravoltas positivas dão aquele alívio e esperança no final! Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)mudar o rumo da hist[oó]ria(.*)|(.*)mudar*hist[oó]ria",
+                ["Alterar o curso da história pode mudar o mundo! Você prefere histórias que tratam de grandes eventos históricos ou mudanças pequenas que geram grandes impactos?",
+                "Essas mudanças são sempre um risco. Você curte mais histórias que tratam de grandes eventos históricos ou mudanças pequenas que geram grandes impactos?"]
+                ],
+                    [
+                    r"(.*)grandes eventos hist[oó]ricos(.*)|(.*)grandes eventos(.*)",
+                    ["Recriar grandes eventos históricos com ficção é uma forma incrível de refletir sobre o passado. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)mudan[çc]as pequenas.*grandes impactos(.*)",
+                    ["Adoro essas mudanças sutis que transformam tudo! Pequenas ações, grandes consequências. Qual outro gênero você gosta?"]
+                ],
+
+        [
+        r"(.*)contato com extraterrestres?(.*)",
+        ["Contatos com extraterrestres são cheios de mistério e possibilidades! Você prefere quando os aliens são hostis ou quando são seres enigmáticos que vêm em paz?",
+        "Esse tema sempre dá margem pra teorias incríveis! Você curte mais histórias quando os aliens são hostis ou quando são seres enigmáticos que vêm em paz?"]
+        ],
+            [
+            r"(.*)aliens? hostis(.*)|hostis",
+            ["Invasões alienígenas sempre rendem ação e tensão! Você prefere quando a humanidade luta de forma heroica ou quando está completamente indefesa?",
+            "Histórias com aliens hostis são cheias de adrenalina. Você curte mais quando a humanidade luta de forma heroica ou quando está completamente indefesa?"]
+            ],
+                [
+                r"(.*)luta de forma heroica(.*)|luta heroica",
+                ["A resistência humana é sempre inspiradora! Você prefere histórias com alianças improváveis ou com heróis solitários que mudam tudo?",
+                "Lutas épicas sempre empolgam. Você curte mais histórias com alianças improváveis ou com heróis solitários que mudam tudo?"]
+                ],
+                    [
+                    r"(.*)alian[çc]as improv[aá]veis(.*)|(.*)alian[cç]a improv[aá]vel(.*)",
+                    ["Alianças improváveis sempre trazem boas surpresas e grandes reviravoltas! Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)her[oó]is solit[aá]rios(.*)",
+                    ["Heróis solitários mostram força e vulnerabilidade de forma única. Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)humanidade indefesa(.*)",
+                ["Quando a humanidade está indefesa, tudo vira um suspense angustiante. Você prefere histórias com fuga e sobrevivência ou com busca desesperada por ajuda externa?",
+                "Esse tipo de narrativa deixa a tensão no ar. Você curte mais histórias com fuga e sobrevivência ou com busca desesperada por ajuda externa?"]
+                ],
+                    [
+                    r"(.*)fuga*sobreviv[eê]ncia(.*)",
+                    ["Narrativas de fuga e sobrevivência sempre prendem a atenção! Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)busca desesperada por ajuda externa(.*)|(.*)ajuda externa(.*)",
+                    ["Essa busca por ajuda externa dá um tom desesperador e emocionante à história. Qual outro gênero você gosta?"]
+                    ],
+            [
+            r"(.*)aliens? enigm[aá]ticos?|aliens? pac[ií]ficos(.*)|pac[ií]ficos?|(.*)enigm[áa]ticos?",
+            ["Aliens misteriosos levantam mil questões! Você prefere quando eles compartilham conhecimento ou quando deixam pistas para serem decifradas?",
+            "Esse tipo de história sempre tem uma vibe filosófica. Você curte mais quando eles compartilham conhecimento ou quando deixam pistas para serem decifradas?"]
+            ],
+                [
+                r"(.*)compartilham? conhecimentos(.*)",
+                ["Aliens que compartilham conhecimento sempre mudam a visão da humanidade. Você prefere quando isso desperta união global ou quando gera caos por medo do desconhecido?",
+                "O impacto desse contato é profundo. Você curte mais quando isso desperta união global ou quando gera caos por medo do desconhecido?"]
+                ],
+                    [
+                    r"(.*)uni[aã]o global(.*)",
+                    ["A união global diante do desconhecido é um símbolo forte de esperança. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)caos por medo do desconhecido(.*)|(.*)medo*desconhecido(.*)",
+                    ["O medo do desconhecido gerando caos é um espelho da própria humanidade. Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)pistas para serem decifradas(.*)|(.*)pistas*decifradas(.*)",
+                ["Pistas alienígenas são puro mistério! Você prefere tramas em que um grupo investiga aos poucos ou histórias com simbolismos que só fazem sentido no final?",
+                "Essas histórias são cheias de enigmas. Você curte mais tramas em que um grupo investiga aos poucos ou histórias com simbolismos que só fazem sentido no final?"]
+                ],
+                    [
+                    r"(.*)investiga aos poucos(.*)",
+                    ["Investigações coletivas criam aquele suspense gostoso que vai crescendo aos poucos. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)simbolismos*no final(.*)",
+                    ["Simbolismos que só se revelam no final deixam a gente pensando por dias! Qual outro gênero você gosta?"]
+                    ],  
+        [
+        r"(.*)futuro dist[oó]pico(.*)|(.*)distopia",
+        ["Futuros distópicos mostram o lado sombrio da humanidade. Você prefere tramas com governos totalitários ou com sociedades colapsadas tentando sobreviver?",
+        "Distopias nos fazem refletir sobre o presente. Você gosta mais de histórias com governos totalitários ou com sociedades colapsadas tentando sobreviver?"]
+        ],
+            [
+            r"(.*)governos? totalit[aá]rios(.*)",
+            ["Governos opressores criam cenários tensos e cheios de vigilância. Você prefere rebeliões contra o sistema ou personagens tentando sobreviver sem chamar atenção?",
+            "Distopias com regimes autoritários são bem intensas. Você curte mais rebeliões contra o sistema ou personagens tentando sobreviver sem chamar atenção?"]
+            ],
+                [
+                r"(.*)rebeli[oõ]es contra o sistema(.*)|rebeli[õo]es",
+                ["Rebeliões distópicas são sempre cheias de adrenalina. Você prefere revoluções em grande escala ou pequenos grupos agindo nas sombras?",
+                "Essa luta contra o sistema é simbólica. Você gosta mais de revoluções em grande escala ou pequenos grupos agindo nas sombras?"]
+                ],
+                    [
+                    r"(.*)revolu[cç][oõ]es em grande escala(.*)",
+                    ["Revoluções em grande escala sempre carregam ação, emoção e ideais fortes. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)pequenos grupos agindo nas sombras(.*)|pequenos? grupos?",
+                    ["Pequenos grupos agindo nas sombras trazem aquele suspense estratégico! Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)sobreviver sem chamar aten[cç][aã]o(.*)",
+                ["Sobreviver em silêncio num regime opressor é tenso e angustiante. Você prefere histórias de opressão ditatorial ou ações furtivas e planejadas?",
+                "Essas tramas mostram o lado humano diante da opressão. Você curte mais histórias de opressão ditatorial ou ações furtivas e planejadas?"]
+                ],
+                    [
+                    r"(.*)opress[aã]o ditatorial(.*)",
+                    ["Narrativas com opressão ditatorial são intensas e fazem a gente refletir muito. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)a[cç][oõ]es furtivas e planejadas(.*)",
+                    ["Ações furtivas e planejadas criam uma tensão constante no ar. Qual outro gênero você gosta?"]
+                    ],
+            [
+            r"(.*)sociedades? colapsadas(.*)",
+            ["Cenários de colapso social mostram o instinto de sobrevivência humano! Você prefere grupos tentando reconstruir uma nova ordem ou disputas violentas por recursos?",
+            "Essas histórias mostram o caos e a esperança. Você curte mais grupos tentando reconstruir uma nova ordem ou disputas violentas por recursos?"]
+            ],
+                [
+                r"(.*)reconstruir uma nova ordem(.*)",
+                ["Reconstruir após o caos é um tema cheio de esperança. Você prefere histórias com líderes carismáticos tentando unir as pessoas ou com comunidades divididas aprendendo a cooperar?",
+                "Esse recomeço é sempre complexo. Você curte mais histórias com líderes carismáticos tentando unir as pessoas ou com comunidades divididas aprendendo a cooperar?"]
+                ],
+                    [
+                    r"(.*)l[ií]deres carism[aá]ticos(.*)",
+                    ["Líderes carismáticos tentando unir as pessoas sempre inspiram! Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)comunidades divididas(.*)|(.*)aprendendo a cooperar(.*)",
+                    ["Ver comunidades se unindo aos poucos é sempre uma jornada tocante. Qual outro gênero você gosta?"]
+                    ],
+                [
+                r"(.*)disputas por recursos(.*)",
+                ["Lutas por recursos escassos mostram o lado mais cru da humanidade. Você prefere conflitos brutais por sobrevivência ou alianças estratégicas que não duram muito?",
+                "Essas histórias são intensas e realistas. Você curte mais conflitos brutais por sobrevivência ou alianças estratégicas que não duram muito?"]
+                ],
+                    [
+                    r"(.*)conflitos brutais por sobreviv[eê]ncia(.*)|conflitos brutais",
+                    ["Conflitos brutais por recursos mostram o lado mais intenso da luta pela vida. Qual outro gênero você gosta?"]
+                    ],
+                    [
+                    r"(.*)alian[çc]as estrat[eé]gicas.*n[aã]o duram(.*)",
+                    ["Alianças que parecem boas, mas logo desmoronam, deixam a trama cheia de reviravoltas. Qual outro gênero você gosta?"]
+                    ],
+]
